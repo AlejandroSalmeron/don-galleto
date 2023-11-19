8dba7824f2c3f6736e79dc15b76cc15380a09d23
alejandroSalmeron <yahirmaster955@gmail.com>
2023-11-18 19:16:59 -0600

    Start the proyect aDon galleto

---
+++ donGalleto_core/src/org/utl/idgs/cuidador/controller/pruebas.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package org.utl.idgs.cuidador.controller;
+
+/**
+ *
+ * @author yahir
+ */
+public class pruebas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //AQUI SOLO IRAN  INSTANCIAS DE LAS FUNCIONES
+    }
+    
+    //AQUI SE HARAN LAS FUNCIONES
+    
+}
